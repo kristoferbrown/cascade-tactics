1798038691d60b55bae6bd2d1835b2b02be15561
kristoferbrown <kristoferbrown@gmail.com>
2019-03-30 17:50:56 -0700

    Basic Result display

--- src/components/MapMenu/AttackResults/AttackResults.js
+++ src/components/MapMenu/AttackResults/AttackResults.js
@@ -6,18 +6,39 @@ export default class AttackResults extends PureComponent {
 
 	render() {
 		const { attackResult, attackUsed, defenseResult } = this.props;
-		const didHit = attackResult.toHit >= defenseResult.passiveDef;
+		const didHit = attackResult.toHit >= defenseResult;
 		console.log(didHit ? 'HIT!!!!' : 'Miss...', attackUsed, attackResult, defenseResult);
 		return (
 			<div 
 				className={classNames({
 					'attackResults': true,
-					'attackResults_attack_str': attackUsed.attribute === 'Strength',
-					'attackResults_attack_fin': attackUsed.attribute === 'Finesse',
-					'attackResults_attack_per': attackUsed.attribute === 'Perception'
+					'attackResults_str': attackUsed.attribute === 'Strength',
+					'attackResults_fin': attackUsed.attribute === 'Finesse',
+					'attackResults_per': attackUsed.attribute === 'Perception'
 				})}
 			>
-				<div>{didHit ? 'HIT!!!' : 'Miss...'}</div>
+				<div className="attackResults_title">{didHit ? 'Hit' : 'Miss'}</div>
+				<div className="attackResults_attackRolls">
+					<div className="attackResults_toHitRoll">
+						Attack: <span className="attackResults_detailStat"> {attackResult.toHit}</span>
+					</div>
+					<div className={classNames({
+						'attackResults_defenseRoll': true,
+						'attackResults_defenseRoll_agi': attackUsed.attribute === 'Strength' || attackUsed.attribute === 'Finesse',
+						'attackResults_defenseRoll_wit': attackUsed.attribute === 'Perception',
+					})}>
+						<div className="attackResults_defenseRollDetail">
+							Defense: <span className="attackResults_detailStat"> {defenseResult}</span>
+						</div>
+					</div>
+				</div>
+				{ didHit && 
+					<div className={classNames({
+						'attackResults_title': true,
+						'attackResults_title_str': attackUsed.attribute !== 'Perception',
+					})}>
+						<span className="attackResults_detailStat">{attackResult.damage}</span> Damage
+					</div> }
 			</div>
 		);
 	}
