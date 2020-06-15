68eebed5d0435084e82d05a95b8c589255c14589
Kris Brown <kbrown@zipwhip.com>
2020-06-15 14:02:29 -0700

    starting to implement menu traces

---
+++ src/components/CharacterMenu/MenuTrace/MenuTrace.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import './MenuTrace.scss';
+
+export default class MenuTrace extends PureComponent {
+	render() {
+		return (
+			<div className={'MenuTrace'}>
+			</div>
+		);
+	}
+}
--- src/components/CharacterMenu/screens/MenuCombatScreen/MenuCombatScreen.js
+++ src/components/CharacterMenu/screens/MenuCombatScreen/MenuCombatScreen.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import CharacterContext from '../../../../context/CharacterContext';
+import MenuTrace from '../../MenuTrace/MenuTrace'
 import './MenuCombatScreen.scss';
 
 export default class MenuCombatScreen extends PureComponent {
@@ -9,6 +10,7 @@ export default class MenuCombatScreen extends PureComponent {
 		return (
 			<div className={'MenuCombatScreen'}>
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_upper'}></div>
+				<MenuTrace coordinates={[{x:100, y: 20}, {x:200, y: 20}]} />
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_lower'}></div>
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_left'}></div>
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_right'}></div>
