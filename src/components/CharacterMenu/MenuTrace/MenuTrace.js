13adb29fea5c178ea5fa7432ae1c6363c405fca2
Kris Brown <kbrown@zipwhip.com>
2020-07-01 17:13:34 -0700

    menutrace cleanup part1

--- src/components/CharacterMenu/MenuTrace/MenuTrace.js
+++ src/components/CharacterMenu/MenuTrace/MenuTrace.js
@@ -5,25 +5,31 @@ import './MenuTrace.scss';
 export default class MenuTrace extends PureComponent {
 	constructor(props) {
 		super(props);
+		const { segments } = this.props;
 		this.segmentList = [];
 		this.currentLeftPos = 0;
 		this.currentRightPos = 0;
 		this.currentTopPos = 0;
 		this.currentBottomPos = 0;
-		this.props.segments.forEach((segment, index) => {
+		const animationMultiplier = segments.length === 2 ? 3 : 2;
+		segments.forEach((segment, index) => {
 			const { isRTL, isBTT } = this.props;
 			const { isDiagonal, isHorizontal, isVertical, length } = segment;
 			this.segmentList.push(
 				<div 
 					className={classNames({
 						'MenuTrace_segment': true,
+						'vertical': isVertical,
 						'diagonalDown': isDiagonal && !isBTT,
 						'diagonalUp': isDiagonal && isBTT,
 						'horizontal': isHorizontal,
-						'vertical': isVertical,
+						'horizontalRTL': isHorizontal && isRTL,
+						'diagonalUpRTL': isDiagonal && isBTT && isRTL,
+						'diagonalDownRTL': isDiagonal && !isBTT && isRTL,
 					})}
 					style={{
-						animationDelay: `0.${index}s`,
+						animationDelay: `0.${index*animationMultiplier}s`,
+						animationDuration: `0.${index*animationMultiplier ? index*animationMultiplier : animationMultiplier}s`,
 						left: isRTL ? 'auto' : this.currentLeftPos,
 						right: isRTL ? this.currentRightPos : 'auto',
 						top: isBTT ? 'auto' : this.currentTopPos,
@@ -32,8 +38,8 @@ export default class MenuTrace extends PureComponent {
 					}}
 				/>
 			);
-			let offsetX = isDiagonal ? length/2 : length;
-			let offsetY = isDiagonal ? length/2+10 : length;
+			let offsetX = isDiagonal ? (length/2)*1.2 : length;
+			let offsetY = isDiagonal ? (length/2)*1.2 : length;
 			if (isHorizontal || isDiagonal) {
 				if (isRTL) {
 					this.currentRightPos = this.currentRightPos+offsetX;
--- src/components/CharacterMenu/screens/MenuCombatScreen/MenuCombatScreen.js
+++ src/components/CharacterMenu/screens/MenuCombatScreen/MenuCombatScreen.js
@@ -9,6 +9,7 @@ export default class MenuCombatScreen extends PureComponent {
 		// const { menuCharacter } = this.context;
 		return (
 			<div className={'MenuCombatScreen'}>
+
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_upper'}></div>
 				<MenuTrace
 					isRTL={false}
@@ -19,10 +20,34 @@ export default class MenuCombatScreen extends PureComponent {
 						{ isHorizontal: true, length: 200},
 						{ isDiagonal: true, length: 65},
 						{ isHorizontal: true, length: 250},
-					]} 
+					]}
 				/>
+
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_lower'}></div>
+				<MenuTrace
+					isRTL={false}
+					isBTT={false}
+					originX={'50%'}
+					originY={321}
+					segments={[
+						{ isDiagonal: true, length: 470},
+						{ isHorizontal: true, length: 300},
+					]}
+				/>
+
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_left'}></div>
+				<MenuTrace
+					isRTL={true}
+					isBTT={false}
+					originX={'40%'}
+					originY={161}
+					segments={[
+						{ isHorizontal: true, length: 150},
+						{ isDiagonal: true, length: 65},
+						{ isHorizontal: true, length: 200},
+					]}
+				/>
+
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_right'}></div>
 				<MenuTrace
 					isRTL={false}
@@ -35,17 +60,40 @@ export default class MenuCombatScreen extends PureComponent {
 						{ isHorizontal: true, length: 200},
 					]} 
 				/>
+
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_legs'}></div>
+				<MenuTrace
+					isRTL={true}
+					isBTT={false}
+					originX={'45%'}
+					originY={650}
+					segments={[
+						{ isDiagonal: true, length: 100},
+						{ isHorizontal: true, length: 500},
+					]}
+				/>
 
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_leftAttack'}></div>
+				<MenuTrace
+					isRTL={true}
+					isBTT={false}
+					originX={'30%'}
+					originY={300}
+					segments={[
+						{ isDiagonal: true, length: 173},
+						{ isHorizontal: true, length: 200},
+					]}
+				/>
+
 				<div className={'MenuCombatScreen_locDetails MenuCombatScreen_rightAttack'}></div>
 				<MenuTrace
 					isRTL={false}
 					isBTT={false}
-					originX={'68%'}
-					originY={316}
+					originX={'69%'}
+					originY={310}
 					segments={[
-						{ isDiagonal: true, length: 200},
+						{ isDiagonal: true, length: 175},
+						{ isHorizontal: true, length: 200},
 					]} 
 				/>
 			</div>
